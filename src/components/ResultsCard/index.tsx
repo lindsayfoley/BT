284d3f83bee377bd88d98e3c89701190c5c0c255
Lindsay Foley <lindsay.foley@outlook.com>
2021-01-31 23:39:18 +0000

    santising descriptions

--- src/components/ResultsCard/index.tsx
+++ src/components/ResultsCard/index.tsx
@@ -1,4 +1,6 @@
 import { FunctionComponent } from 'react';
+import DOMPurify from 'dompurify';
+
 interface ISource {
   id: string;
   name: string;
@@ -13,7 +15,8 @@ export interface IArticle {
 }
 
 export const CHARACTER_LIMIT = 150;
-export const truncateText = (text: string) => `${text.slice(0, CHARACTER_LIMIT)}...`;
+export const truncateText = (text: string) =>
+  `${text.slice(0, CHARACTER_LIMIT)}...`;
 
 const ResultsCard: FunctionComponent<IArticle> = (article: IArticle) => {
   const {
@@ -25,7 +28,15 @@ const ResultsCard: FunctionComponent<IArticle> = (article: IArticle) => {
     source: { name, id },
   } = article;
 
-  const truncatedDescription = description && truncateText(description);
+  const sanitsedDescription = DOMPurify.sanitize(description, {
+    USE_PROFILES: { html: true },
+  });
+
+  if (sanitsedDescription.length === 0) {
+    return null;
+  }
+
+  const truncatedDescription = truncateText(sanitsedDescription);
 
   return (
     <>
@@ -33,7 +44,10 @@ const ResultsCard: FunctionComponent<IArticle> = (article: IArticle) => {
         <h3>{title}</h3>
         <span className="author">{author || name || id}</span>
         <p>
-          <span className="description" dangerouslySetInnerHTML={{ __html: truncatedDescription }} />
+          <span
+            className="description"
+            dangerouslySetInnerHTML={{ __html: truncatedDescription }}
+          />
           <a target="_blank" rel="noreferrer" href={url}>
             read more &gt;
           </a>
